1f07dae3bb435a59925037c94680d60b68afab76
LEQ-Projects <110405296+LEQ-Projects@users.noreply.github.com>
2022-08-01 11:44:35 -0300

    Add files via upload

--- funcionalidades.js
+++ funcionalidades.js
@@ -13,3 +13,51 @@ function search() {
         }
     }
 }
+
+function filterR() {
+    let input = "reagente"
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('linha');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display= "";
+                          
+        }
+    }
+}
+
+function filterV() {
+    let input = "vidraçaria"
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('linha');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display= "";
+                          
+        }
+    }
+}
+
+function filterG() {
+    let input = ""
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('linha');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display= "";
+                          
+        }
+    }
+}

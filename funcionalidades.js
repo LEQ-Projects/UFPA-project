e75a6f7aec2e26bb3e1ac7cc73e7c73833dc3bdb
LEQ-Projects <110405296+LEQ-Projects@users.noreply.github.com>
2022-08-12 08:56:29 -0300

    Add files via upload
    Adição da aba "Histórico" e da funcionalidade de esconder a tabela principal para apresentar outras.

--- funcionalidades.js
+++ funcionalidades.js
@@ -25,6 +25,9 @@ function filterR() {
         }
         else {
             x[i].style.display= "";
+            document.getElementById('tabelaF').style.display = 'grid';
+            document.getElementById('pesquisa').style.display = 'grid';
+            document.getElementById('tabelaHist').style.display = 'none';
                           
         }
     }
@@ -41,6 +44,9 @@ function filterV() {
         }
         else {
             x[i].style.display= "";
+            document.getElementById('tabelaF').style.display = 'grid';
+            document.getElementById('pesquisa').style.display = 'grid';
+            document.getElementById('tabelaHist').style.display = 'none';
                           
         }
     }
@@ -52,12 +58,32 @@ function filterG() {
     let x = document.getElementsByClassName('linha');
       
     for (i = 0; i < x.length; i++) { 
+        var display = document.getElementById('tabelaF').style.display;
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
             x[i].style.display="none";
-        }
+        }        
         else {
             x[i].style.display= "";
+            document.getElementById('tabelaF').style.display = 'grid';
+            document.getElementById('pesquisa').style.display = 'grid';
+            document.getElementById('tabelaHist').style.display = 'none';
                           
         }
     }
+}
+
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    var display2 = document.getElementById('pesquisa');
+    if(display == "none" && display2 =="none"){
+        document.getElementById(el).style.display = 'block';
+        
+        }
+    else{ 
+        document.getElementById(el).style.display = 'none';}
+        document.getElementById('pesquisa').style.display = 'none';
+        document.getElementById('tabelaHist').style.display = 'grid';}
+
+function showIt() {
+    document.getElementById('desc').style.visibility = 'visible';
 }
